53b0885a7b6ad2a10ff3c73603ee6b4778e39d1f
Lucas Silvestre <lukas.silvestre@gmail.com>
2012-08-16 19:55:09 -0300

    First commit

---
+++ src/curso/ufmg/MeuApActivity.java
@@ -0,0 +1,13 @@
+package curso.ufmg;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MeuApActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
